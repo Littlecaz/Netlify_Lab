d2ea7f9f1d3a4c219457c499db8c7265b10152e3
Littlecaz <75811569+Littlecaz@users.noreply.github.com>
2021-03-10 12:05:03 -0500

    Added the js file to fix error

---
+++ css/.eleventy.js
@@ -0,0 +1,19 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('css')
+    return {
+      passthroughFileCopy: true
+    }
+  }
+  module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('img');
+    return {
+      passthroughFileCopy: true
+    }
+  }
+  const CleanCSS = require("clean-css");
+  module.exports = function(eleventyConfig) {
+    eleventyConfig.addFilter("cssmin", function(code) {
+      return new CleanCSS({}).minify(code).styles;
+    });
+  };
