da254399387b706d2747f5e6a409ba3364e61eb4
Littlecaz <75811569+Littlecaz@users.noreply.github.com>
2021-03-11 09:45:46 -0500

    removed duped code

--- _site/css/.eleventy.js
+++ _site/css/.eleventy.js
@@ -1,10 +1,4 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('css')
-    return {
-      passthroughFileCopy: true
-    }
-  }
-  module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy('img');
     return {
